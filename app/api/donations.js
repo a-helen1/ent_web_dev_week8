12ba873681c817965b8cb946e841eb736f79baa9
a-helen1 <andrewhelen1@gmail.com>
2020-05-30 11:38:50 +0100

    steps 1 & 2

---
+++ app/api/donations.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Donation = require('../models/donation');
+const boom = require('@hapi/boom');
+
+const Donations = {
+  findAll: {
+    auth: false,
+    handler: async function (request, h) {
+      const donations = await Donation.find();
+      return donations;
+    }
+  },
+
+  findByCandidate: {
+    auth: false,
+    handler: async function (request, h) {
+      const donations = await Donation.find({ candidate: request.params.id});
+      return donations;
+    }
+  }
+};
+
+module.exports = Donations;
--- routes-api.js
+++ routes-api.js
@@ -1,5 +1,6 @@
 const Candidates = require('./app/api/candidates');
 const Users = require('./app/api/users');
+const Donations = require('./app/api/donations.js');
 
 module.exports = [
   {method: 'GET', path: '/api/candidates', config: Candidates.find },
@@ -7,10 +8,14 @@ module.exports = [
   {method: 'POST', path: '/api/candidates', config: Candidates.create },
   {method: 'DELETE', path: '/api/candidates/{id}', config: Candidates.deleteOne },
   {method: 'DELETE', path: '/api/candidates', config: Candidates.deleteAll },
+  {method: 'GET', path: '/api/candidates/{id}/donations', config: Donations.findByCandidate },
 
   {method: 'GET', path: '/api/users', config: Users.find },
   {method: 'GET', path: '/api/users/{id}', config: Users.findOne },
   {method: 'POST', path: '/api/users', config: Users.create },
   {method: 'DELETE', path: '/api/users', config: Users.deleteAll },
   {method: 'DELETE', path: '/api/users/{id}', config: Users.deleteOne },
+
+  {method: 'GET', path: '/api/donations', config: Donations.findAll },
+
 ]
