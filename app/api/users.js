3b4d0c1d3904b703f1247d77e980bedbce5c6ce0
a-helen1 <andrewhelen1@gmail.com>
2020-06-01 22:19:24 +0100

    end of lab 12b

--- app/api/users.js
+++ app/api/users.js
@@ -6,7 +6,9 @@ const utils = require('./utils.js');
 
 const Users = {
   find: {
-    auth: false,
+    auth: {
+      strategy: 'jwt',
+    },
     handler: async function(request, h) {
       const users = await User.find();
       return users;
@@ -14,7 +16,9 @@ const Users = {
   },
 
   findOne: {
-    auth: false,
+    auth: {
+      strategy: 'jwt',
+    },
     handler: async function( request, h) {
       try {
         const user = await User.findOne({ _id: request.params.id });
@@ -29,7 +33,7 @@ const Users = {
   },
 
   create: {
-    auth:false,
+    auth: false,
     handler: async function (request, h) {
       const newUser = new User(request.payload);
       const user = await newUser.save();
@@ -41,7 +45,9 @@ const Users = {
   },
 
   deleteAll: {
-    auth:false,
+    auth: {
+      strategy: 'jwt',
+    },
     handler: async function (request, h) {
       await User.deleteMany({});
       return { success: true };
@@ -49,7 +55,9 @@ const Users = {
   },
 
   deleteOne: {
-    auth: false,
+    auth: {
+      strategy: 'jwt',
+    },
     handler: async function(request, h) {
       const response = await User.deleteOne({_id: request.params.id });
       if (user) {
@@ -61,14 +69,17 @@ const Users = {
 
   authenticate: {
     auth: false,
-    handler: async function(request, h) {
+    handler: async function (request, h) {
       try {
         const user = await User.findOne({ email: request.payload.email });
         if (!user) {
-          return Boom.notFound('Authentication failed. User not found');
+          return Boom.unauthorized('User not found');
+        } else if (user.password !== request.payload.password) {
+          return Boom.unauthorized('Invalid password');
+        } else {
+          const token = utils.createToken(user);
+          return h.response({ success: true, token: token }).code(201);
         }
-        const token = utils.createToken(user);
-        return h.response({ success: true, token: token }).code(201);
       } catch (err) {
         return Boom.notFound('internal db failure');
       }
